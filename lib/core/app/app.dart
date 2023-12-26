802d5ada993a46d64fe932fd8bf21c571f8e1967
asuracoder91 <asuracoder@gmail.com>
2023-12-26 23:37:20 +0900

    feat: create main app

---
+++ lib/core/app/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Modora extends StatelessWidget {
+  const Modora({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Modora',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
